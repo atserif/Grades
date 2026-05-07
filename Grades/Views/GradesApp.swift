f37e3f50587906a4ff8f1d6175a200f55485549a
atserif <aram.soneson@icloud.com>
2026-05-07 08:36:57 -0400

    Initial commit after previous Git breakage

---
+++ Grades/Views/GradesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradesApp.swift
+//  Grades
+//
+//  Created by Aram Soneson on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct GradesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Grades/Views/SemesterView.swift
@@ -0,0 +1,18 @@
+//
+//  SemesterView.swift
+//  Grades
+//
+//  Created by Aram Soneson on 5/7/26.
+//
+
+import SwiftUI
+
+struct SemesterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SemesterView()
+}
