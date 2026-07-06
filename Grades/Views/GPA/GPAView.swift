aa51a948678524898591ed8bc7de10b3e7883294
atserif <aram.soneson@icloud.com>
2026-07-06 11:15:22 -0400

    Overhaul toolbar layout and functionality to fix animations and add capability to edit mode

--- Grades/Views/GPA/GPAView.swift
+++ Grades/Views/GPA/GPAView.swift
@@ -13,9 +13,6 @@ struct GPAView: View {
 
 	@State private var selection: Set<UUID> = []
 	@State private var editState: EditMode = .inactive
-	@State private var initialCourses: [Course] = (1...7).map { _ in
-		Course(name: "New Course", grade: .A, level: .regular)
-	}
 	@State private var courses: [Course] = (1...7).map { _ in
 		Course(name: "New Course", grade: .A, level: .regular)
 	}
@@ -39,128 +36,122 @@ struct GPAView: View {
 	
 	var body: some View {
 		NavigationStack {
-			if courses.isEmpty {
-				ContentUnavailableView("No Courses", systemImage: "graduationcap", description: Text("Your courses will appear here."))
-					.navigationTitle(navigationTitle)
-					.toolbarTitleDisplayMode(.inlineLarge)
-					#if os(iOS)
-					.background(Color(.systemGroupedBackground))
-					#endif
-					.toolbar {
-						ToolbarItemGroup(placement: .topBarTrailing) {
-//							Menu("More", systemImage: "ellipsis") {
-								Button("New Course", systemImage: "plus") {
-									withAnimation {
-										let newCourse = Course(name: "New Course", grade: .A, level: .regular)
-										courses.append(newCourse)
-									}
-								}
-								.disabled(courses.count >= 50)
-								
-								Button("Reset", systemImage: "arrow.clockwise") {
-									withAnimation {
-										courses = initialCourses
-									}
-								}
-								.disabled(courses.elementsEqual(initialCourses) { $0.name == $1.name && $0.grade == $1.grade && $0.level == $1.level })
-//							}
-						}
-					}
-			} else {
-				List(selection: $selection) {
+			List(selection: $selection) {
+				if !courses.isEmpty {
 					switch horizontalSizeClass {
 					case .regular:
-						RegularGPAContent(courses: $courses, unweightedGPA: unweightedGPA, weightedGPA: weightedGPA)
+						RegularGPAContent(editState: $editState, courses: $courses, unweightedGPA: unweightedGPA, weightedGPA: weightedGPA)
 					case .compact, .none, .some:
-						CompactGPAContent(courses: $courses, unweightedGPA: unweightedGPA, weightedGPA: weightedGPA)
+						CompactGPAContent(editState: $editState, courses: $courses, unweightedGPA: unweightedGPA, weightedGPA: weightedGPA)
 					}
 				}
-				.formStyle(.grouped)
-				.navigationTitle(navigationTitle)
-				.toolbarTitleDisplayMode(.inlineLarge)
-				.scrollDismissesKeyboard(.interactively)
-				.animation(.default, value: editState)
-				.animation(.default, value: selection)
-				.environment(\.editMode, $editState)
-				.toolbarVisibility(editState == .inactive ? .automatic : .hidden, for: .tabBar)
-				.toolbarVisibility(editState == .inactive ? .hidden : .automatic, for: .bottomBar)
-				.toolbar {
-					if editState == .inactive {
+			}
+			.formStyle(.grouped)
+			.navigationTitle(navigationTitle)
+			.toolbarTitleDisplayMode(.inlineLarge)
+			.scrollDismissesKeyboard(.interactively)
+			.environment(\.editMode, $editState)
+			.toolbarVisibility(editState == .inactive ? .automatic : .hidden, for: .tabBar)
+			.toolbarVisibility(editState == .inactive ? .hidden : .automatic, for: .bottomBar)
+			.overlay {
+				if courses.isEmpty {
+					ContentUnavailableView {
+						Label("No Courses", systemImage: "graduationcap.fill")
+					} description: {
+						Text("Your courses will appear here.")
+					}
+					.background(Color(.systemGroupedBackground))
+				}
+			}
+			.toolbar {
+				if editState == .inactive {
+					ToolbarItem(placement: .topBarTrailing) {
+						Button("Select") {
+							withAnimation {
+								selection.removeAll()
+								editState = .active
+							}
+						}
+						.disabled(courses.isEmpty)
+					}
+					
+					ToolbarSpacer(.fixed, placement: .topBarTrailing)
+					
+					ToolbarItemGroup(placement: .primaryAction) {
+//						Menu("More", systemImage: "ellipsis") {
+							Button("New Course", systemImage: "plus") {
+								withAnimation {
+									let newCourse = Course(name: "New Course", grade: .A, level: .regular)
+									courses.insert(newCourse, at: 0)
+								}
+							}
+							.disabled(courses.count >= 50)
+							
+							Button("Reset", systemImage: "arrow.clockwise") {
+								withAnimation {
+									courses = (1...7).map { _ in
+										Course(name: "New Course", grade: .A, level: .regular)
+									}
+								}
+							}
+//						}
+					}
+				} else {
+					if selection.count == courses.count {
 						ToolbarItem(placement: .topBarTrailing) {
-							Button("Select") {
+							Button("Deselect All") {
 								withAnimation {
-									editState = .active
-								}
-							}
-						}
-						
-						ToolbarSpacer(.fixed, placement: .topBarTrailing)
-						
-						ToolbarItemGroup(placement: .topBarTrailing) {
-//							Menu("More", systemImage: "ellipsis") {
-								Button("New Course", systemImage: "plus") {
-									withAnimation {
-										let newCourse = Course(name: "New Course", grade: .A, level: .regular)
-										courses.append(newCourse)
-									}
-								}
-								.disabled(courses.count >= 50)
-								
-								Button("Reset", systemImage: "arrow.clockwise") {
-									withAnimation {
-										courses = initialCourses
-									}
-								}
-								.disabled(courses.elementsEqual(initialCourses) { $0.name == $1.name && $0.grade == $1.grade && $0.level == $1.level })
-//							}
-						}
-					} else {
-						if selection.count == courses.count {
-							ToolbarItem(placement: .topBarTrailing) {
-								Button("Deselect All") {
-									withAnimation {
-										selection.removeAll()
-									}
-								}
-							}
-						} else {
-							ToolbarItem(placement: .topBarTrailing) {
-								Button("Select All") {
-									withAnimation {
-										selection = Set(courses.map { $0.id })
-									}
-								}
-							}
-						}
-						
-						ToolbarItem(placement: .confirmationAction) {
-							Button("Confirm", systemImage: "checkmark", role: .confirm) {
-								withAnimation {
-									editState = .inactive
 									selection.removeAll()
 								}
 							}
 						}
-					}
-					
-					ToolbarSpacer(.flexible, placement: .bottomBar)
-					
-					ToolbarItem(placement: .bottomBar) {
-						Button("Delete", systemImage: "trash", role: .destructive) {
-							withAnimation {
-								courses.removeAll { course in
-									selection.contains(course.id)
-								}
-								
-								selection.removeAll()
-								
-								if courses.isEmpty {
-									editState = .inactive
+					} else {
+						ToolbarItem(placement: .topBarTrailing) {
+							Button("Select All") {
+								withAnimation {
+									selection = Set(courses.map { $0.id })
 								}
 							}
 						}
-						.disabled(selection.count <= 0)
 					}
+					
+					ToolbarItem(placement: .confirmationAction) {
+						Button("Confirm", systemImage: "checkmark", role: .confirm) {
+							withAnimation {
+								editState = .inactive
+								selection.removeAll()
+							}
+						}
+					}
+				}
+				
+				ToolbarItem(placement: .bottomBar) {
+					Button("Reset", systemImage: "arrow.clockwise") {
+						withAnimation {
+							courses = (1...7).map { _ in
+								Course(name: "New Course", grade: .A, level: .regular)
+							}
+						}
+					}
+				}
+				
+				ToolbarSpacer(.flexible, placement: .bottomBar)
+				
+				ToolbarItem(placement: .bottomBar) {
+					Button("Delete", systemImage: "trash", role: .destructive) {
+						withAnimation {
+							courses.removeAll { course in
+								selection.contains(course.id)
+							}
+							
+							selection.removeAll()
+							
+							if courses.isEmpty {
+								editState = .inactive
+							}
+						}
+					}
+					.disabled(selection.count <= 0)
 				}
 			}
 		}
