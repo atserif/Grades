271257fa05b8a9ad4ba3bcdeacdae2e4dc49453b
atserif <aram.soneson@icloud.com>
2026-07-10 12:18:50 -0400

    Rename reusable views folder

---
+++ Grades/Views/Common/CopyableRow.swift
@@ -0,0 +1,77 @@
+//
+//  CopyableRow.swift
+//  Grades
+//
+//  Created by Aram Soneson on 6/5/26.
+//
+
+import SwiftUI
+
+struct CopyableRow: View {
+	let label: String
+	let value: String
+	@State private var hovered: Bool = false
+	@State private var clicked: Bool = false
+	
+	var body: some View {
+		#if os(macOS)
+		HStack {
+			Text(label)
+			
+			Spacer()
+			
+			Button {
+				if !clicked {
+					withAnimation(.easeInOut(duration: 0.3)) {
+						Copy.copyToClipboard(value)
+						clicked = true
+					}
+					
+					DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+						withAnimation(.easeInOut(duration: 0.3)) {
+							clicked = false
+						}
+					}
+				}
+			} label: {
+				// TODO: Fix hover effect flickering while animation is playing
+				Label {
+					Text(clicked ? "Copied" : value)
+				} icon: {
+					if clicked {
+						Image(systemName: "document.on.document.fill")
+							.transition(.move(edge: .trailing).combined(with: .opacity).combined(with: .blurReplace))
+					}
+				}
+				.padding(clicked ? 7 : 5)
+				.labelIconToTitleSpacing(4)
+				.foregroundStyle(.secondary)
+				.background(clicked ? Color(.tertiarySystemFill) : hovered ? Color(.tertiarySystemFill) : .clear)
+				.clipShape(.rect(cornerRadius: clicked ? 9 : 7))
+				.contentTransition(.numericText())
+				.animation(.default, value: value)
+				.monospacedDigit()
+				.onHover {
+					hovered = $0
+				}
+			}
+			.buttonStyle(UnresponsiveButtonStyle())
+			.offset(x: clicked ? 0 : -2)
+			.padding(-7)
+		}
+		#else
+		LabeledContent(label) {
+			Text(value)
+				.monospacedDigit()
+		}
+		.contentTransition(.numericText())
+		.animation(.default, value: value)
+		.contextMenu {
+			Button("Copy \(label)", systemImage: "document.on.document") {
+				Copy.copyToClipboard(value)
+			}
+		}
+		#endif
+	}
+}
+
---
+++ Grades/Views/Common/StaticNavigationTitle.swift
@@ -0,0 +1,38 @@
+//
+//  StaticNavigationTitle.swift
+//  Grades
+//
+//  Created by Aram Soneson on 7/10/26.
+//
+
+import SwiftUI
+
+struct StaticNavigationTitle: View {
+	var title: LocalizedStringKey
+	
+    var body: some View {
+		HStack {
+			Text(title)
+				.font(.largeTitle)
+				.bold()
+				.fixedSize()
+			
+			Spacer()
+		}
+    }
+}
+
+#Preview {
+	NavigationStack {
+		ScrollView {
+			Text("Hello, world!")
+		}
+		.toolbar {
+			ToolbarItem(placement: .title) {
+				StaticNavigationTitle(title: "Title")
+			}
+		}
+		.toolbarTitleDisplayMode(.inline)
+		.toolbarRole(.editor)
+	}
+}
