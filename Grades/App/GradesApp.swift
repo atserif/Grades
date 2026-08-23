e4543dacf9aeeffb05c9f203b9550b2348ca5e0d
atserif <aram.soneson@icloud.com>
2026-08-22 22:01:25 -0400

    Add version and build number to Settings bundle

--- Grades/App/GradesApp.swift
+++ Grades/App/GradesApp.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 @main
 struct GradesApp: App {
+	init() {
+		if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
+			UserDefaults.standard.set("\(version) (\(build))", forKey: "version")
+		}
+	}
+	
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
